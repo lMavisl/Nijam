ca0c7e52dcbd62460f2573d0c2ae054c1fa4c7c3
lMavisl <61184766+lMavisl@users.noreply.github.com>
2020-02-18 17:48:37 +0800

    added comments

--- Test.py
+++ Test.py
@@ -6,5 +6,7 @@ plotter = pv.Plotter()    # instantiate the plotter
 plotter.add_volume(mesh, cmap='jet')    # add volume
 
 #plotter.add_mesh_threshold(mesh)  # add mesh threshold
+
+#Untick below statement to save file from vtk to vti
 #mesh.save('sarimage.vti', binary=False)
 cpos = plotter.show()     # show the rendering window
